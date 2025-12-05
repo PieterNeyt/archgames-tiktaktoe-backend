13c0a89ef9ac699191c3d49fdcabb76521c9e65d
axel peeter <axel.peeters.1@student.kdg.be>
2025-12-05 10:06:12 +0100

    @value

--- src/main/java/be/kdg/ip3/tttbackend/portal/ai/AiClient.java
+++ src/main/java/be/kdg/ip3/tttbackend/portal/ai/AiClient.java
@@ -1,16 +1,20 @@
 package be.kdg.ip3.tttbackend.portal.ai;
 
 import be.kdg.ip3.tttbackend.api.dto.AiGameStateDto;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+
 @Component
 public class AiClient {
     private final RestTemplate restTemplate;
-    private final String aiServiceUrl = "http://localhost:3333/ai-player/move";
+    @Value("${ai.service.url}")
+    private String aiServiceUrl;
 
     public AiClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;

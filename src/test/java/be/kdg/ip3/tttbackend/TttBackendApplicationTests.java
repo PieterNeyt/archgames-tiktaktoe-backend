f2034975efef514bc9e2c427c9e0626da0fa5630
Cian Van Acker <cian.vanacker@student.kdg.be>
2026-01-02 19:18:23 +0100

    test fix

--- src/test/java/be/kdg/ip3/tttbackend/TttBackendApplicationTests.java
+++ src/test/java/be/kdg/ip3/tttbackend/TttBackendApplicationTests.java
@@ -1,12 +1,16 @@
 package be.kdg.ip3.tttbackend;
 
+import be.kdg.ip3.tttbackend.portal.messaging.sender.tttMessagePublisher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @Import(TestContainersConfig.class)
 class TttBackendApplicationTests {
+    @MockitoBean
+    private tttMessagePublisher tttMessagePublisher;
 
     @Test
     void contextLoads() {
